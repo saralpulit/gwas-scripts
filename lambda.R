a6446c42c36d541d20b7465ed190bbde85d3ded1
Sara L Pulit <sara.pulit@gmail.com>
2017-10-14 11:05:31 +0200

    Create lambda.R

---
+++ lambda.R
@@ -0,0 +1,21 @@
+## calculate genomic inflation (lambda) in your data set
+
+input=commandArgs(trailingOnly=T)[1]
+stat_type=commandArgs(trailingOnly=T)[2] ## P, Z, CHISQ
+
+## Reads data
+## Header should have column "P"
+
+S <- read.table(input,header=T)
+if (stat_type == "Z")
+z=S$P
+
+if (stat_type == "CHISQ")
+z=sqrt(S$P)
+
+if (stat_type == "P")
+z=qnorm(S$P/2)
+
+## calculates lambda
+lambda = round(median(z^2,na.rm=T)/qchisq(0.5,df=1),3)
+print(lambda)
